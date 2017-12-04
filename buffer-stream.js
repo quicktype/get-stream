67ae4ddfbac117cd1fd403f2b9e6c14e3c221d88
David Siegel <djsiegel@gmail.com>
2017-12-04 23:08:51 +0700

    Port to TypeScript and emit ES5

--- buffer-stream.js
+++ buffer-stream.js
@@ -1,51 +1,41 @@
-'use strict';
-const PassThrough = require('stream').PassThrough;
-
-module.exports = opts => {
-	opts = Object.assign({}, opts);
-
-	const array = opts.array;
-	let encoding = opts.encoding;
-	const buffer = encoding === 'buffer';
-	let objectMode = false;
-
-	if (array) {
-		objectMode = !(encoding || buffer);
-	} else {
-		encoding = encoding || 'utf8';
-	}
-
-	if (buffer) {
-		encoding = null;
-	}
-
-	let len = 0;
-	const ret = [];
-	const stream = new PassThrough({objectMode});
-
-	if (encoding) {
-		stream.setEncoding(encoding);
-	}
-
-	stream.on('data', chunk => {
-		ret.push(chunk);
-
-		if (objectMode) {
-			len = ret.length;
-		} else {
-			len += chunk.length;
-		}
-	});
-
-	stream.getBufferedValue = () => {
-		if (array) {
-			return ret;
-		}
-
-		return buffer ? Buffer.concat(ret, len) : ret.join('');
-	};
-
-	stream.getBufferedLength = () => len;
-
-	return stream;
-};
+"use strict";
+var PassThrough = require("stream").PassThrough;
+export default function bufferStream(opts) {
+    opts = Object.assign({}, opts);
+    var array = opts.array;
+    var encoding = opts.encoding;
+    var buffer = encoding === "buffer";
+    var objectMode = false;
+    if (array) {
+        objectMode = !(encoding || buffer);
+    }
+    else {
+        encoding = encoding || "utf8";
+    }
+    if (buffer) {
+        encoding = null;
+    }
+    var len = 0;
+    var ret = [];
+    var stream = new PassThrough({ objectMode: objectMode });
+    if (encoding) {
+        stream.setEncoding(encoding);
+    }
+    stream.on("data", function (chunk) {
+        ret.push(chunk);
+        if (objectMode) {
+            len = ret.length;
+        }
+        else {
+            len += chunk.length;
+        }
+    });
+    stream.getBufferedValue = function () {
+        if (array) {
+            return ret;
+        }
+        return buffer ? Buffer.concat(ret, len) : ret.join("");
+    };
+    stream.getBufferedLength = function () { return len; };
+    return stream;
+}
--- index.js
+++ index.js
@@ -1,51 +1,45 @@
-'use strict';
-const bufferStream = require('./buffer-stream');
-
+"use strict";
+var bufferStream = require("./buffer-stream");
 function getStream(inputStream, opts) {
-	if (!inputStream) {
-		return Promise.reject(new Error('Expected a stream'));
-	}
-
-	opts = Object.assign({maxBuffer: Infinity}, opts);
-
-	const maxBuffer = opts.maxBuffer;
-	let stream;
-	let clean;
-
-	const p = new Promise((resolve, reject) => {
-		const error = err => {
-			if (err) { // null check
-				err.bufferedData = stream.getBufferedValue();
-			}
-
-			reject(err);
-		};
-
-		stream = bufferStream(opts);
-		inputStream.once('error', error);
-		inputStream.pipe(stream);
-
-		stream.on('data', () => {
-			if (stream.getBufferedLength() > maxBuffer) {
-				reject(new Error('maxBuffer exceeded'));
-			}
-		});
-		stream.once('error', error);
-		stream.on('end', resolve);
-
-		clean = () => {
-			// some streams doesn't implement the `stream.Readable` interface correctly
-			if (inputStream.unpipe) {
-				inputStream.unpipe(stream);
-			}
-		};
-	});
-
-	p.then(clean, clean);
-
-	return p.then(() => stream.getBufferedValue());
+    if (!inputStream) {
+        return Promise.reject(new Error("Expected a stream"));
+    }
+    opts = Object.assign({ maxBuffer: Infinity }, opts);
+    var maxBuffer = opts.maxBuffer;
+    var stream;
+    var clean;
+    var p = new Promise(function (resolve, reject) {
+        var error = function (err) {
+            if (err) {
+                // null check
+                err.bufferedData = stream.getBufferedValue();
+            }
+            reject(err);
+        };
+        stream = bufferStream(opts);
+        inputStream.once("error", error);
+        inputStream.pipe(stream);
+        stream.on("data", function () {
+            if (stream.getBufferedLength() > maxBuffer) {
+                reject(new Error("maxBuffer exceeded"));
+            }
+        });
+        stream.once("error", error);
+        stream.on("end", resolve);
+        clean = function () {
+            // some streams doesn't implement the `stream.Readable` interface correctly
+            if (inputStream.unpipe) {
+                inputStream.unpipe(stream);
+            }
+        };
+    });
+    p.then(clean, clean);
+    return p.then(function () { return stream.getBufferedValue(); });
 }
-
 module.exports = getStream;
-module.exports.buffer = (stream, opts) => getStream(stream, Object.assign({}, opts, {encoding: 'buffer'}));
-module.exports.array = (stream, opts) => getStream(stream, Object.assign({}, opts, {array: true}));
+module.exports.buffer = function (stream, opts) {
+    return getStream(stream, Object.assign({}, opts, { encoding: "buffer" }));
+};
+module.exports.array = function (stream, opts) {
+    return getStream(stream, Object.assign({}, opts, { array: true }));
+};
